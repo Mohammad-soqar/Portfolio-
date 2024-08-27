3a2c9a6c343795e940ca750357eafe296f192cf3
Mohammad Soqar <mnsoqar1@gmail.com>
2024-08-27 12:39:33 +0300

    fix images

--- src/components/sidepanelData.js
+++ src/components/sidepanelData.js
@@ -1,38 +1,38 @@
 export const data = {
   Images: {
-    profilePic: "/images/MohammadSoqar.png",
-    star: "/images/Star.png",
+    profilePic: "./images/MohammadSoqar.png",
+    star: "./images/Star.png",
     //background
-    WebsiteBackground: "/images/Background.png",
+    WebsiteBackground: "./images/Background.png",
     //tools
-    flutter: "/images/tech/Flutter.png",//
-    dart: "/images/tech/Dart.png",//
-    python: "/images/tech/Python.png",//
-    colab: "/images/tech/Colab.png",//
-    chatgpt: "/images/tech/ChatGPT.png",//
-    mysql: "/images/tech/MySql.png",//
-    firebase: "/images/tech/FireBase.png",//////
-    mongodb: "/images/tech/Mongodb.png",//
-    git: "/images/tech/Git.png",//
-    github: "/images/tech/Github.png",//
-    nodejs: "/images/tech/Nodejs.png",//
-    csharp: "/images/tech/Cs.png",//
-    aspnet: "/images/tech/Aspnet.png",//
-    react: "/images/tech/React.png",//
-    js: "/images/tech/JS.png",//
-    figma: "/images/tech/Figma.png",//
-    illustrator: "/images/tech/illustrator.png",//
-    photoshop: "/images/tech/PhotoShop.png",//
-    flutter2: "/images/tech/Flutter2.png",//
-    dart2: "/images/tech/Dart2.png",//
-    python2: "/images/tech/Python2.png",//
-    chatgpt2: "/images/tech/ChatGPT2.png",//
-    firebase2: "/images/tech/FireBase2.png",//
-    mongodb2: "/images/tech/Mongodb2.png",//
-    nodejs2: "/images/tech/Nodejs2.png",//
-    aspnet2: "/images/tech/Aspnet2.png",//
-    react2: "/images/tech/React2.png",//
-    figma2: "/images/tech/Figma2.png",//
+    flutter: "./images/tech/Flutter.png",//
+    dart: "./images/tech/Dart.png",//
+    python: "./images/tech/Python.png",//
+    colab: "./images/tech/Colab.png",//
+    chatgpt: "./images/tech/ChatGPT.png",//
+    mysql: "./images/tech/MySql.png",//
+    firebase: "./images/tech/FireBase.png",//////
+    mongodb: "./images/tech/Mongodb.png",//
+    git: "./images/tech/Git.png",//
+    github: "./images/tech/Github.png",//
+    nodejs: "./images/tech/Nodejs.png",//
+    csharp: "./images/tech/Cs.png",//
+    aspnet: "./images/tech/Aspnet.png",//
+    react: "./images/tech/React.png",//
+    js: "./images/tech/JS.png",//
+    figma: "./images/tech/Figma.png",//
+    illustrator: "./images/tech/illustrator.png",//
+    photoshop: "./images/tech/PhotoShop.png",//
+    flutter2: "./images/tech/Flutter2.png",//
+    dart2: "./images/tech/Dart2.png",//
+    python2: "./images/tech/Python2.png",//
+    chatgpt2: "./images/tech/ChatGPT2.png",//
+    firebase2: "./images/tech/FireBase2.png",//
+    mongodb2: "./images/tech/Mongodb2.png",//
+    nodejs2: "./images/tech/Nodejs2.png",//
+    aspnet2: "./images/tech/Aspnet2.png",//
+    react2: "./images/tech/React2.png",//
+    figma2: "./images/tech/Figma2.png",//
 
   },
 
@@ -51,7 +51,7 @@ export const data = {
       color: "rgba(42, 248, 155, 0.2)",
       summary: "This social media platform enhances user interaction and community engagement by offering customizable profiles, diverse content-sharing options, and real-time notifications. Users can connect meaningfully, participate in discussions, and maintain control over their privacy settings. The app is designed to foster connections and keep users actively involved with their network.",
       projectBenefits: "LocaLink enhances the social media experience by fostering deeper user engagement and community interaction. It offers customizable profiles and diverse content-sharing options, allowing users to connect with others and participate in meaningful discussions. With real-time notifications and privacy controls, LocaLink ensures an engaging and personalized experience, making it a valuable tool for building connections and staying connected.",
-      images: ['/images/projects/LocaLink.png', '/images/projects/LocaLink-2.png', '/images/projects/LocaLink-3.png'],
+      images: ['./images/projects/LocaLink.png', './images/projects/LocaLink-2.png', './images/projects/LocaLink-3.png'],
       projectLink: 'https://my-portfolio.com',
     },
     {
@@ -67,7 +67,7 @@ export const data = {
       color: "rgba(255, 223, 186, 0.2)",
       summary: "This e-commerce platform for e-books offers a streamlined shopping experience, featuring a user-friendly interface for browsing and purchasing digital books. Designed for ease of use, Bookworm ensures a smooth process for book enthusiasts looking to explore and buy e-books.",
       projectBenefits: "Bookworm simplifies the process of buying e-books with its intuitive interface and efficient browsing capabilities. It provides a convenient platform for users to find and purchase digital books, enhancing the overall e-commerce experience for book lovers.",
-      images: ["/images/projects/Bookworm.png", "/images/projects/Bookworm-2.png"],
+      images: ["./images/projects/Bookworm.png", "./images/projects/Bookworm-2.png"],
       projectLink: "https://my-portfolio.com"
     },
     {
@@ -83,7 +83,7 @@ export const data = {
       color: "rgba(255, 123, 123, 0.2)",
       summary: "This app facilitates restaurant reservations and pre-ordering of food, ensuring that meals are ready when users arrive. With QR code integration for easy payment and ordering, Onifood enhances the efficiency and convenience of dining out.",
       projectBenefits: "Onifood streamlines the dining experience by enabling users to reserve tables, place orders in advance, and handle payments seamlessly through QR codes. This approach improves restaurant service efficiency and enhances customer satisfaction.",
-      images: ["/images/projects/Onifood.png", "/images/projects/Onifood-2.png"],
+      images: ["./images/projects/Onifood.png", "./images/projects/Onifood-2.png"],
       projectLink: "https://my-portfolio.com"
     },
     {
@@ -99,7 +99,7 @@ export const data = {
       color: "rgba(255, 206, 86, 0.2)",
       summary: "This system offers a comprehensive solution for managing travel and tourism operations, including car rentals and trip bookings. With an integrated CRM and support features, it streamlines various aspects of tourism management and content handling.",
       projectBenefits: "Zoom Tourism simplifies travel management by integrating car rentals, trip bookings, and content management into a single platform. Its CRM capabilities and support features enhance operational efficiency and improve customer service.",
-      images: ["/images/projects/ZoomTourism.png", "/images/projects/ZoomTourism-2.png"],
+      images: ["./images/projects/ZoomTourism.png", "./images/projects/ZoomTourism-2.png"],
       projectLink: "https://my-portfolio.com"
     },
     // Add more projects as needed
@@ -107,28 +107,28 @@ export const data = {
 
   socialData: [
     {
-      icon: "/images/SocialMedia/Instagram.png",
-      icon_big: "/images/SocialMedia/Instagram-big.png",
+      icon: "./images/SocialMedia/Instagram.png",
+      icon_big: "./images/SocialMedia/Instagram-big.png",
       link: "https://www.instagram.com/mohammad_soqar/"
     },
     {
-      icon: "/images/SocialMedia/Github.png",
-      icon_big: "/images/SocialMedia/Github-big.png",
+      icon: "./images/SocialMedia/Github.png",
+      icon_big: "./images/SocialMedia/Github-big.png",
       link: "https://github.com/Mohammad-soqar"
     },
     {
-      icon: "/images/SocialMedia/LinkedIn.png",
-      icon_big: "/images/SocialMedia/LinkedIn-big.png",
+      icon: "./images/SocialMedia/LinkedIn.png",
+      icon_big: "./images/SocialMedia/LinkedIn-big.png",
       link: "https://www.linkedin.com/in/mohammad-soqar-ahmad/"
     },
     {
-      icon: "/images/SocialMedia/YouTube.png",
-      icon_big: "/images/SocialMedia/YouTube-big.png",
+      icon: "./images/SocialMedia/YouTube.png",
+      icon_big: "./images/SocialMedia/YouTube-big.png",
       link: "https://www.youtube.com/@mohammadsoqar2809/featured"
     },
     {
-      icon: "/images/SocialMedia/Behance.png",
-      icon_big: "/images/SocialMedia/Behance-big.png",
+      icon: "./images/SocialMedia/Behance.png",
+      icon_big: "./images/SocialMedia/Behance-big.png",
       link: "https://www.behance.net/mohammadsoqar"
     }
   ],
